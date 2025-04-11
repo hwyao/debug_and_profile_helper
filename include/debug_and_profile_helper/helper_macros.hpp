8ced5258432e5be6954183258446fd77ca700997
Haowen Yao <haowen.yao@tum.de>
2025-04-11 16:30:34 +0200

    simplify the ROS compile and simplify fix some minor cmake bug.

--- include/debug_and_profile_helper/helper_macros.hpp
+++ include/debug_and_profile_helper/helper_macros.hpp
@@ -1,6 +1,8 @@
 #ifndef DEBUG_AND_PROFILE_HELPER__HELPER_MACROS_
 #define DEBUG_AND_PROFILE_HELPER__HELPER_MACROS_
 
+#include "debug_and_profile_helper/config.h"
+
 #include <string>
 #include <chrono>
 #include "debug_and_profile_helper/helper_class.hpp"
