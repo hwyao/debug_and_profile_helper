7cd00f86ac727d0e592c8db5ab208132859aefd4
Haowen Yao <haowen.yao@tum.de>
2025-04-15 15:12:01 +0200

    minor bugfix: fix the problem of Eigen and build flag.

--- include/debug_and_profile_helper/helper_macros.hpp
+++ include/debug_and_profile_helper/helper_macros.hpp
@@ -1,12 +1,10 @@
 #ifndef DEBUG_AND_PROFILE_HELPER__HELPER_MACROS_
 #define DEBUG_AND_PROFILE_HELPER__HELPER_MACROS_
 
-#include "debug_and_profile_helper/config.h"
+#include "debug_and_profile_helper/helper_class.hpp"
 
 #include <string>
 #include <chrono>
-#include "debug_and_profile_helper/helper_class.hpp"
-
 #include <iostream>
 
 //#define DBGNPROF_USE_FILE
